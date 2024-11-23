03b7029b33101162f3b5dd9ceac1aa1506d32a5e
kalyan joshi <joshi1307kashyap@gmail.com>
2024-11-23 22:07:32 +0530

    C lang

---
+++ subtractt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    int sub;
+    printf("Enter the number :");
+    scanf("%d %d", &a,&b);
+    sub = a-b;
+    printf("Sub: %d", sub);
+    return 0;
+}
