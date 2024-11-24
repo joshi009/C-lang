af95d6e7c70aff58e357c0f75ca3c8ae26ef1f4f
kalyan joshi <joshi1307kashyap@gmail.com>
2024-11-24 13:01:50 +0530

    c lang

---
+++ Multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    int mult;
+    printf("Enter the number");
+    scanf("%d %d",&a, &b );
+    mult = a*b;
+    printf("multiply: %d", mult);
+    return 0;
+}
