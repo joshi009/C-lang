2dc965b6994de440ea30300ee3b32aa53e3ef916
kalyan joshi <joshi1307kashyap@gmail.com>
2024-11-27 11:15:28 +0530

    c lang

---
+++ Divide.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a = 8;
+    int b = 2;
+    int div;
+    // printf("Enter the Number");
+    // scanf("%d %d",&a,&b);
+    div = a/b;
+    printf("divide: %d", div);
+    return 0;
+}
