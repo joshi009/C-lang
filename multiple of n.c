eb807feb55b36f2902b9611c9be93df0a4659230
kalyan joshi <joshi1307kashyap@gmail.com>
2024-12-10 10:32:22 +0530

    c lang

---
+++ multiple of n.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    // int n;
+    // printf("Enter the number ");
+    // scanf("%d ", &n);
+    for(int i=7; i<100; i+=7){
+        printf("%d ",i);
+    }
+    return 0;
+}
