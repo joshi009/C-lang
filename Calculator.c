d018e18a38f3d6b3e9e2811cbd3b35ba454be995
kalyan joshi <joshi1307kashyap@gmail.com>
2024-12-14 18:09:09 +0530

    c lang

--- Calculator.c
+++ Calculator.c
@@ -3,23 +3,29 @@ int main(){
     char ch ;
     int a;
     int b;
-    printf("Enter the two  number");
-    scanf("%d,%d",&a,&b);
+    printf("Enter the first  number :");
+    scanf("%d",&a);
+    printf("Enter the second number :");
+    scanf("%d",&b);
+    printf("Enter the operation + , - , * , / ");
+    getchar();
+    scanf("%c",&ch);
     switch(ch){
         case '-':
-        printf(a-b);
+        printf("Result: %d\n",a-b);
         break;
         case '+':
-        printf(a+b);
+        printf("Result: %d\n",a+b);
         break;
         case '*':
-        printf(a*b);
+        printf("Result: %d\n",a*b);
         break;
         case '/':
-        printf(a/b);
+        printf("Result: %d\n",a/b);
         break;
         case '%':
-        printf(a%b);
+        printf("Result: %d\n",a%b);
         break;
     }
+    return 0;
 }
